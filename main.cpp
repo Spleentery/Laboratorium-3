4a6fc753b4bfb28b3230ab58ff31538d5619f675
david <david78787878787@gmail.com>
2022-01-05 22:52:33 +0100

    Obsługa błedów (обработка исключений) try,throw,catch
     Program powinien następnie spróbować przekonwertować wprowadzony tekst na liczbę typu int. W trakcie konwersji program może zwrócić wyjątek: Wprowadzony ciąg zawiera nieprawidłowe znaki. Na sam koniec program powinien wypisać skonwertowaną liczbę na ekran.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
+bool isNumber(const string& str)
+{
+    for (char const &c : str) {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
 int main() {
-    cout << "Enter two numbers: ";
-    int num;
-    int num2;
-    cin >> num;
-    cin >> num2;
+    cout << "Enter TEXT: ";
+    string text;
+    cin >> text;
 
     try{
-        int c = num/num2;
-        if(num%num2 ==0 ){
-            cout << "result is integer " << c << endl;
+           if(isNumber(text) == true){
+            cout << "result is integer " << stoi(text)<< endl;
         }
         else{
-            throw "result is double";
+            throw "result is not int";
         }
     }
     catch(const char*) {
-        cerr << "We caught an const char* exception.\nCan't divide " << num << " by " << num2 << endl;
+        cerr << "We caught an const char* exception.\nThe TEXT is NOT an integer :(\n" << text << endl;
     }
     return 0;
 }
