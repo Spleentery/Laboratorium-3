3fd83b40e48c04bb535dac33bad828c086cb9efd
david <david78787878787@gmail.com>
2022-01-05 22:24:49 +0100

    Obsługa błedów (обработка исключений) try,throw,catch
     program wczytuje od użytkownika dwie liczby naturalne a i b. Program ma wypisać na ekran wynik dzielenia a przez b, a jeśli jest to niemożliwe to ma wypisać odpowiedni komunikat. Dzielenie będzie odbywać się w zbiorze liczb naturalnych, dlatego wynikiem dzielenia nie może być ułamek.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-#include "libraryA.h"
-
-
 using namespace std;
-int add (int a, int b) {
-        return a + b;
-    }
-
-int adding (int a, int b, int &c) {
-    return c = a + b;
-}
-
-double pole(double a,double b, double &c ) {
-    return c = a * b;
-}
-double pole(int a, int b, int c, double &p) {
-    return p = 2*c*(a+b);
-}
 
 int main() {
-    std::cout << "Hello" << std::endl;
-    sayHello(std::cout);
-    meow();
-    int k;
-    double p,o;
-    pole(4,5,p);
-      cout << p << endl;
-    pole(4,5,10,p);
-    cout << p << endl;
-    adding(5,9, k);
-    cout << k << endl;
-    int w = add(1,2);
-    cout << w << endl;
+    cout << "Enter two numbers: ";
+    int num;
+    int num2;
+    cin >> num;
+    cin >> num2;
+
+    try{
+        int c = num/num2;
+        if(num%num2 ==0 ){
+            cout << "result is integer " << c << endl;
+        }
+        else{
+            throw "result is double";
+        }
+    }
+    catch(const char*) {
+        cerr << "We caught an const char* exception.\nCan't divide " << num << " by " << num2 << endl;
+    }
     return 0;
 }
